8814b27121e71db465d05a5c25d0542fbe944483
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-08-15 17:20:48 +0400

    Update 1-fifo_cache.py

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-"""FIFO caching module
+"""FIFO caching module.
 """
 
 from base_caching import BaseCaching
 
+
 class FIFOCache(BaseCaching):
-    """FIFO cache system"""
-    
+    """FIFO cache system."""
+
     def __init__(self):
-        """Initialize"""
+        """Initialize the FIFO cache."""
         super().__init__()
-        self.order = [] 
-        
+        self.order = []
+
     def put(self, key, item):
-        """Add an item to the cache"""
+        """Add an item to the cache."""
         if key is None or item is None:
-            return 
-            
+            return
+
         if key in self.cache_data:
             self.cache_data[key] = item
             return
-            
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             oldest_key = self.order.pop(0)
             del self.cache_data[oldest_key]
             print("DISCARD: {}".format(oldest_key))
-            
+
         self.cache_data[key] = item
         self.order.append(key)
 
     def get(self, key):
-        """Get an item by key"""
+        """Get an item by key."""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]

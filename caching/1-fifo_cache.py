86a074e8e9d4903e62ecddc87e1101caab46d778
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-08-15 17:13:49 +0400

    Update 1-fifo_cache.py

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""FIFO caching module"""
 
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
     def __init__(self):
+         """Initialize"""
         super().__init__()
         self.order = [] 
     def put(self, key, item):
+        """Add an item to the cache"""
         if key is None or item is None:
             return 
         if key in self.cache_data:
@@ -20,6 +23,7 @@ class FIFOCache(BaseCaching):
         self.order.append(key)
 
     def get(self, key):
+        """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]

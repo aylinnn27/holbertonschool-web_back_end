96ffb314483f7b8c2c9fbee594da404a1c861043
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-09-03 11:45:00 +0400

    Update filtered_logger.py

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,17 +3,24 @@
 Module for filtering sensitive data from log messages
 """
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+    message: str, separator: str) -> str:
     """
     Obfuscates the values of specified fields in a log message.
     """
     pattern = f"({'|'.join(fields)})=.*?{separator}"
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}{separator}", message)
+    return re.sub(pattern,
+        lambda m: f"{m.group(1)}={redaction}{separator}",
+        message)
 
 fields = ["password", "date_of_birth"]
 messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
 
 for message in messages:
     print(filter_datum(fields, 'xxx', message, ';'))
+
+
+
+

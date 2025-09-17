f9f8238d099ddfb172ca76416c7cb78da18f5f0e
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-09-17 23:13:17 +0400

    Update app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,7 +7,9 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from flask import Flask, jsonify
 
+app = Flask(__name__)
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,3 +27,7 @@ if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
+# 401 Unauthorized error handler
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({"error": "Unauthorized"}), 401

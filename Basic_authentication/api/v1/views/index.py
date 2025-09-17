aae51c91909821467a7e779be74577859749ccb9
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-09-17 23:22:57 +0400

    Update index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+"""Module of Index views for API v1"""
 
 from flask import jsonify, abort
-from api.v1.views import app_views  # imported blueprint
+from api.v1.views import app_views  # use the imported blueprint
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
-      - the status of the API
+        - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
-      - the number of each objects
+        - the number of each object
     """
     from models.user import User
-    stats = {}
-    stats['users'] = User.count()
+    stats = {"users": User.count()}
     return jsonify(stats)
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_route() -> str:
-    """ GET /api/v1/unauthorized
+    """GET /api/v1/unauthorized
     Trigger 401 Unauthorized error
     """
-    abort(401)  # This triggers the 401 error handler in app.py
+    abort(401)

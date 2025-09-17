1ec8d8432be5e568b885b079f235c54b90d769a9
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-09-17 23:29:39 +0400

    Update index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
-"""Module of Index views for API v1"""
-
+"""
+Module of Index views
+"""
 from flask import jsonify, abort
-from api.v1.views import app_views  # use the imported blueprint
+from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """GET /api/v1/status
+    """
+    GET /api/v1/status
     Return:
-        - the status of the API
+      - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """GET /api/v1/stats
-    Return:
-        - the number of each object
-    """
-    from models.user import User
-    stats = {"users": User.count()}
-    return jsonify(stats)
-
-
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_route() -> str:
-    """GET /api/v1/unauthorized
-    Trigger 401 Unauthorized error
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Return:
+      - raises a 403 error
+    """
+    abort(403)

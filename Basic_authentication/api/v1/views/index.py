70ec8027771bc65f77efaae9d342431508b37d81
aylinnn27 <aylinguliyeva17224@gmail.com>
2025-09-17 23:35:26 +0400

    Update index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""
+Module of Index views
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """
+    GET /api/v1/status
     Return:
       - the status of the API
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """
+    GET /api/v1/stats
     Return:
       - the number of each objects
     """
@@ -23,10 +28,12 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
-    """ GET /api/v1/unauthorized
-    Return:
-      - Aborts with a 401 Unauthorized error
+    """
+    GET /api/v1/unauthorized
+    Raise:
+      - a 401 Unauthorized error
     """
     abort(401)
